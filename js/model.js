f84a33eeaf7330dbdfb24850556deb238161483b
rimay07 <rickysadiwa@gmail.com>
2018-02-19 18:55:22 +0800

    Initialize divs and buttons
    Create divs and button functionality to handle single page click and game continuity

---
+++ js/model.js
@@ -0,0 +1,31 @@
+var Model = function(){
+	this.questions = [
+		{
+			"question" : "test 1",
+			"options" : {
+				"a" : "a1",
+				"b" : "b1",
+				"c" : "c1",
+				"d" : "d1"
+			}
+		},
+		{
+			"question" : "test 2",
+			"options" : {
+				"a" : "a2",
+				"b" : "b2",
+				"c" : "c2",
+				"d" : "d2"
+			}
+		},
+		{
+			"question" : "test 3",
+			"options" : {
+				"a" : "a3",
+				"b" : "b3",
+				"c" : "c3",
+				"d" : "d3"
+			}
+		}
+	];
+}
